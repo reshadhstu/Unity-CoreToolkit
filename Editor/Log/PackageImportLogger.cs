e5894d699be24e09162048bc145bb5edbc717cf2
Reshad Bin Kibria <reshad.hstu@gmail.com>
2025-03-28 02:27:31 +0600

    Finished the Logger

--- Editor/Log/PackageImportLogger.cs
+++ Editor/Log/PackageImportLogger.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,45 +6,15 @@ namespace CoreToolkit.Editor.Log
     [InitializeOnLoad]
     public static class PackageImportLogger
     {
-        private static string PackageVersion => GetPackageVersion();
-        private static string ImportKey => $"PackageImported_{PackageVersion}";
+        private const string ImportKey = "PackageImported";
 
         static PackageImportLogger()
         {
             if (!EditorPrefs.HasKey(ImportKey))
             {
-                Debug.Log($"Package Version {PackageVersion} Imported Successfully");
+                Debug.Log("Package Imported Successfully");
                 EditorPrefs.SetBool(ImportKey, true);
             }
-        }
-
-        private static string GetPackageVersion()
-        {
-            // Construct the path to your package.json file
-            string packageJsonPath = Path.Combine("Packages", "CoreToolkit", "package.json");
-
-            // Load the package.json file as a TextAsset
-            TextAsset packageJsonAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(packageJsonPath);
-
-            if (packageJsonAsset != null)
-            {
-                // Parse the JSON content to extract the version
-                string jsonText = packageJsonAsset.text;
-                var jsonObject = JsonUtility.FromJson<PackageJson>(jsonText);
-                return jsonObject.version;
-            }
-            else
-            {
-                Debug.LogWarning("Could not find package.json at " + packageJsonPath);
-                return "Unknown";
-            }
-        }
-
-        // Helper class to deserialize the version field from package.json
-        [System.Serializable]
-        private class PackageJson
-        {
-            public string version;
         }
     }
 }
